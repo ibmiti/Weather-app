6129bf5db89645016e19d38912f67f56134435c3
Dimitri <devebeme@gmail.com>
2019-01-07 10:47:22 -0500

    changing (http://api.openweathermap.org/data/2.5/weather?q=, ...   so that we can get custom location's.. this is core to the functionality of the app

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.name.city.value;
+    const city = e.target.elements.name.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
