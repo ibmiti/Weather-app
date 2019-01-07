3374185ad2ccfec53383193835be85175bc30689
Dimitri <devebeme@gmail.com>
2019-01-07 08:24:39 -0500

    added const city = e.target.elements.name.city.value; this will begin to let us customize the location that we would like to check the weather for.. more coming.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const API_KEY = "181a59f51e6d2f159c8644b30670e4ba";
 class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
+    const city = e.target.elements.name.city.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
