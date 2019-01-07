1256da3ead815bcf4aade823fa591b9960cf27fd
Dimitri <devebeme@gmail.com>
2019-01-07 08:01:54 -0500

    added an e to async when pressing the 'get Weather' button so that it does not by defualt reload entire page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ const API_KEY = "181a59f51e6d2f159c8644b30670e4ba";
 
 
 class App extends React.Component {
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}
       &units=metric`);
 
