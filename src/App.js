061f70933b53919b127f5bc98d0246591583a272
Dimitri mcDaniel <ibmiti@Dimitris-MacBook-Air.local>
2019-01-06 19:34:28 -0500

    making changes in app.js and index.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello!</p>
+        <h1>How are you?</h1>
+      </div>
+    );
+  }
+};
+
+export default App;
