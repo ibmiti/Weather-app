e6989b324c5d8b903dd771215136ca6e08803ba3
Dimitri <devebeme@gmail.com>
2019-01-07 07:51:52 -0500

    fixed errors in compile // the error occured in the inclusion of title route in Form.js file.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+
+const API_KEY = "181a59f51e6d2f159c8644b30670e4ba";
+
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}
+      &units=metric`);
+
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
-        <p>Hello!</p>
-        <h1>How are you?</h1>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
